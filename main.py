4adc5dba495716859a6405332a8dd68de13a082c
Racsok <oscarmcg02@gmail.com>
2026-05-06 20:30:13 -0500

    fix: usar src.manejadores.privado en main.py para que escuche los comandos

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logger = config_logger(__name__)
 
 class TGBuscador:
     def __init__(self) -> None:
-        plugins = dict(root="src.manejadores")
+        plugins = dict(root="src.manejadores") #src.manejadores.privado
         self.bot = Client("TGBuscador", api_id=config.api_id, api_hash=config.api_hash, bot_token=config.bot_token, plugins=plugins)
 
     def run(self):
@@ -17,6 +17,9 @@ class TGBuscador:
 
 if __name__ == "__main__":
     bot = TGBuscador()
-    logger.info("Aplicación iniciando")
-    bot.run()
-    logger.info("Aplicación iniciada")
+    logger.info("Intentando conectar con Telegram...")
+    try:
+        bot.run()
+    except Exception as e:
+        logger.error(f"Error crítico durante la ejecución: {e}")
+    logger.info("Aplicación finalizada")
